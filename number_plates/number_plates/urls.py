c906eb91c3b1b8deed11e6f282036052f0d846e8
Alex-Tsyb <113551674+Alex-Tsyb@users.noreply.github.com>
2024-09-07 21:16:46 +0300

    add urls in app

--- number_plates/number_plates/urls.py
+++ number_plates/number_plates/urls.py
@@ -16,12 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 
-urlpatterns = [
-    path('register/', views.register, name='register'),
-]
