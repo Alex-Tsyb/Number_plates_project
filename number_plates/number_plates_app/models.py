0b8f8148f8e73e69722a6936aac0b531b8541845
ArleKinG44 <142050495+ArleKinG44@users.noreply.github.com>
2024-09-07 12:27:12 +0300

    auth
    login/admin/user

--- number_plates/number_plates_app/models.py
+++ number_plates/number_plates_app/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.username
+
+class Car(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    license_plate = models.CharField(max_length=15)
+    entry_time = models.DateTimeField(auto_now_add=True)
+    exit_time = models.DateTimeField(null=True, blank=True)
+    total_fee = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+
+    def __str__(self):
+        return self.license_plate
