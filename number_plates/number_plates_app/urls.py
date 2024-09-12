f74b0067c8d75ee199713c22648076bdb5239f0d
Alex-Tsyb <113551674+Alex-Tsyb@users.noreply.github.com>
2024-09-12 18:12:35 +0300

    add html, css

--- number_plates/number_plates_app/urls.py
+++ number_plates/number_plates_app/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('register/', views.register, name='register'),
     path('admin/dashboard/', views.admin_dashboard, name='admin_dashboard'),
+    path('tarif/', views.tarif, name='tarif'),
+    path('rules/', views.rules, name='rules'),
 ]
