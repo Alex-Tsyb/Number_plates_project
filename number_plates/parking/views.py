efa71087807305afcb82e5dc67a67336a25cf054
tarsweb <tarsweb@ukr.net>
2024-09-17 19:04:17 +0300

    add session in modal

--- number_plates/parking/views.py
+++ number_plates/parking/views.py
@@ -1,10 +1,13 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
+from django_htmx.http import HttpResponseClientRefresh
 
 from .repository import (
+    get_session_by_id,
     get_places_info,
     generate_admin_statistics,
-    get_session_by_id_for_form,
+    get_data_session_by_id_for_form,
+    get_data_for_new_session_for_form,
 )
 from .forms import SessionForm
 
@@ -42,7 +45,7 @@ def parking_session(request, pk: int = None):
             return redirect(to="parking:index")
     else:
         if not pk is None:
-            object_session = get_session_by_id_for_form(pk)
+            object_session = get_data_session_by_id_for_form(pk)
             form = SessionForm(initial=object_session)
         else:
             form = SessionForm()
@@ -53,20 +56,27 @@ def parking_session(request, pk: int = None):
 
     return render(request, "parking/parking_session.html", context)
 
+
 @login_required
-def parking_session_dialog(request, pk = None):
+def parking_session_dialog(request, pk=None):
 
     if request.method == "POST":
-        form = SessionForm(request = request, data=request.POST, prefix="modal")
+        if not request.POST["modal-session_id"] == "":
+            updated_session = get_session_by_id(int(request.POST["modal-session_id"]))
+            form = SessionForm(data=request.POST, instance=updated_session, prefix="modal")
+        else:
+            form = SessionForm(data=request.POST, prefix="modal")
         if form.is_valid():
             form.save()
-            return redirect(to="parking:index")
+            return HttpResponseClientRefresh()
+
     else:
         if not pk is None:
-            object_session = get_session_by_id_for_form(pk)
-            form = SessionForm(initial=object_session)
+            object_session = get_data_session_by_id_for_form(pk)
+            form = SessionForm(initial=object_session, prefix="modal")
         else:
-            form = SessionForm(prefix="modal")
+            object_new_session = get_data_for_new_session_for_form(request.GET)
+            form = SessionForm(initial=object_new_session, prefix="modal")
 
     context = {}
     context["type"] = "create" if not pk else "edit"
