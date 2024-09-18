c800900e85eef5376fd75ac6fc57c2719edd607e
tarsweb <tarsweb@ukr.net>
2024-09-18 16:44:17 +0300

    media settings

--- number_plates/number_plates/urls.py
+++ number_plates/number_plates/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,5 +26,5 @@ urlpatterns = [
     path('cars/', include('cars.urls' , namespace='cars')),
     path('tarifs/', include('parking_rates.urls' , namespace='tarifs')),
     # path("payment/", include("payment.urls", namespace="payment")),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
